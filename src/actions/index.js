05bac0c930c515006db8b912637d50893617f50d
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-26 15:05:18 -0500

    added header with authentication

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types'
 
 export const saveComment = comment => ({
   type: SAVE_COMMENT,
@@ -10,3 +10,8 @@ export const fetchComments = () => async dispatch => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
   dispatch({ type: FETCH_COMMENTS, payload: data})
 }
+
+export const changeAuth = isLoggedIn => ({
+  type: CHANGE_AUTH,
+  payload: isLoggedIn
+})
