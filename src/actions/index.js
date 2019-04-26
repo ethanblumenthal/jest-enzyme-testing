6430eb033a228dfdb6d2102c94da9210015ab217
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-26 13:25:06 -0500

    implemented button to fetch comments via axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
-import { SAVE_COMMENT } from 'actions/types'
+import axios from 'axios'
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
 
 export const saveComment = comment => ({
   type: SAVE_COMMENT,
   payload: comment
 })
+
+export const fetchComments = () => async dispatch => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
+  dispatch({ type: FETCH_COMMENTS, payload: data})
+}
